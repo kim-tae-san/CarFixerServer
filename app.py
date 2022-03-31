8406313ec074ab60bc74fc1f06e4481b49903167
kimtaesan <ekdlqld21@naver.com>
2022-03-31 21:42:52 +0900

    update

---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+import json
+import urllib
+import h5py
+import pickle as pk
+import numpy as np
+
+from flask import Flask, request
+
+import engine # remember to reinclude this
+
+# A <form> tag is marked with enctype=multipart/form-data and an <input type=file> is placed in that form.
+# The application accesses the file from the files dictionary on the request object.
+# use the save() method of the file to save the file permanently somewhere on the filesystem.
+
+app = Flask(__name__)
+
+@app.route('/assessment', methods=['GET', 'POST'])
+def upload_and_classify():
+	if request.method == 'GET':
+		url = request.args.get('url')
+		print(url)
+		url = url.replace('images/', r'images%2F')
+		print(url)
+		token = request.args.get('token')
+		if len(token) > 0:
+			model_results = engine.engine(url + "&token=" + token)
+		else:
+			model_results = engine.engine(url)
+		return json.dumps(model_results);
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False) # remember to set back to False	
